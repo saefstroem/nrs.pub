c19f7539c5c1731e80d58fe239ff7c7e4c6badc1
Saefstroem <139136798+saefstroem@users.noreply.github.com>
2026-04-25 14:41:10 +0800

    build to repo root

--- src/client/svelte.config.js
+++ src/client/svelte.config.js
@@ -1,6 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter({ fallback: '200.html' }) } };
+const config = { kit: { adapter: adapter({ pages: '../../dist', assets: '../../dist', fallback: '200.html' }) } };
 
 export default config;
